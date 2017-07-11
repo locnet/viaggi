fd652b4ac9f4bf3563d1a4aece06b2cf145a819f
locnet <locnetarganda@gmail.com>
2017-07-11 13:39:35 +0200

    Registro agencia con envio de email de confirmacion

--- app/Http/Controllers/Auth/MailController.php
+++ app/Http/Controllers/Auth/MailController.php
@@ -6,11 +6,12 @@ use Illuminate\Http\Request;
 use App\User;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Mail;
 
 class MailController extends Controller
 {
     /**
-     * Confirmacion del registro nueva agencia.
+     * Mandar el email con las instrucciones para confirmar el registro.
      *
      * @param  string  $confirmation_code
      * 
@@ -19,93 +20,35 @@ class MailController extends Controller
     public function sendConfirmation($confirmation_code) 
     {
         $user = User::where('confirmation_code','=', $confirmation_code)->first();
-        return view('mails.welcome_mail',compact('user'));
+      
+
         Mail::send('mails.welcome_mail', ['user' => $user], function($message)
         {
             $message->to('locnetarganda@gmail.com', 'John Smith')->subject('Welcome!');
         });
     }
+
+    /**
+     * Confirmacion del registro nueva agencia.
+     *
+     * @param  string  $confirmation_code
+     * 
+     * @return \Illuminate\Http\Response
+     */
      
      public function getConfirmation($confirmation_code) 
      {
-        if ($user = User::where('confirmation_code','=', $confirmation_code)->first() ) {
+        $user = User::where('confirmation_code','=', $confirmation_code)->first();
+
+        if ( $user->status === 0) {      // el registro no esta confirmado
+            
             $user->status = 1;
             $user->save();
+            $message = "Gracias, la cuenta ha sido activada corectamente.";
+        } else {
+            $message = "Esta cuenta esta activa, no es necesario activarla de nuevo.";
         }
+        return view('auth.register_message')->with('message',$message);
      }
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+    
 }
--- resources/views/mails/welcome_mail.blade.php
+++ resources/views/mails/welcome_mail.blade.php
@@ -1,4 +1,15 @@
+@extends('master')
 
-<p>Gracias por registrate {{ ucwords($user->name) }},</p>
-<p>para terminar el registro haz clik en el siquiente enlace:</p>
-<p><a href="{{ url('get-confirmation/'.$user->confirmation_code) }}">Enlace</a></p>
+@section('content')
+    <div class="container">
+        <div class="row">
+        	<div class="col-md-8 col-md-offset-2">
+	            <h2 class="lato-300 dark-blue text-center">Gracias por registrate {{ ucwords($user->name) }}</h2>
+				<h4>Para terminar corectamente el proceso de registro de agencia te rogamos que has click en el
+					siguiente enlace. Si el enlace no funciona copia y pega la siquiente direccion en tu 
+					navegador preferido.</h4>
+				<p><a href="{{ url('get-confirmation/'.$user->confirmation_code) }}">Enlace</a></p>
+			</div>
+        </div>
+    </div>
+@endsection
